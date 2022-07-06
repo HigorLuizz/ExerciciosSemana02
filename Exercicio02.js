9ac75037e8c368f9102e258623edb368ec97cdf5
HigorLuizz <higor.luiz1997@gmail.com>
2022-07-06 06:29:23 -0300

    Exercício 03

--- Exercicio02.js
+++
@@ -1,4 +0,0 @@
-var nome = window.prompt("Digite seu nome");
-var email = window.prompt("Digite seu Email");
-document.write("<h4>"+nome+"<h4/>");
-document.write("<h4>"+email+"<h4/>");
---
+++ Exercicio03.js
@@ -0,0 +1,3 @@
+var n1 = parseInt(window.prompt("Digite um número"));
+var n2 = parseInt(window.prompt("Digite outro número"));
+document.write("<h4> O resultado da soma entre "+n1+" e "+n2+" é de "+(n1+n2)+"<h4/>");
