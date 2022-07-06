18aa8cd0c2a023eb49ef1fe87f0fac6ce178ee4f
HigorLuizz <higor.luiz1997@gmail.com>
2022-07-06 19:48:57 -0300

    Exercício 06

--- Exercicio05.js
+++
@@ -1,6 +0,0 @@
-var num = parseInt(window.prompt("Digite um número qualquer"))
-if (num % 2==0){
-    document.write("<h3>O número "+num+" é PAR<h3/>")
-}else{
-    document.write("<h3>O número "+num+" é IMPAR<h3/>")
-}
